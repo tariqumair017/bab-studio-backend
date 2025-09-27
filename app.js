86e9381c6f58668962882979b06ad0931b201eb3
fahad-hafeez78 <fahad.dev9@gmail.com>
2025-09-27 23:41:53 +0500

    Added cors

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const morgan = require("morgan"); 
+const morgan = require("morgan");
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
-const fileUpload = require("express-fileupload"); 
+const fileUpload = require("express-fileupload");
 
 const config = require("./config/config.js");
 const eventRoutes = require("./routes/events.js");
@@ -12,6 +13,27 @@ const connectdb = require("./config/db/index.js");
 const app = express();
 const port = config.PORT;
 
+const allowedOrigins = [
+  'http://localhost:5173',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      console.log('CORS blocked for origin:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
 
 //database connection 
 connectdb();
